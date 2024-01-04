d419e0caea41a7317902268ea1d32268f1b167cb
Ozodbek <ozodbekkarimov100@gmail.com>
2024-01-04 07:52:42 +0100

    new

--- src/string_1/MiddleThree.java
+++ src/string_1/MiddleThree.java
@@ -15,7 +15,8 @@ public class MiddleThree {
 
 
     static String middleThree(String str) {
-        return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+//        return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+        return str;
     }
 
 }
