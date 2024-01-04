0ffd9a22874e277d04262d4633586879ba86edac
Ozodbek <ozodbekkarimov100@gmail.com>
2024-01-04 07:47:18 +0100

    Middle Three solved

---
+++ src/string_1/MiddleThree.java
@@ -0,0 +1,21 @@
+package string_1;
+
+/**
+ * Creator: Karimov Ozodbek
+ * Date 02/02/2023
+ */
+public class MiddleThree {
+
+    public static void main(String[] args) {
+
+        System.out.println(middleThree("Candy"));
+        System.out.println(middleThree("and"));
+        System.out.println(middleThree("solving"));
+    }
+
+
+    static String middleThree(String str) {
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+    }
+
+}
