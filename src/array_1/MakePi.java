18b84a7974c86d75c78228dce539b2b07cd719cb
Ozodbek <ozodbekkarimov100@gmail.com>
2024-01-04 16:48:16 +0100

    MakePi solved

---
+++ src/array_1/MakePi.java
@@ -0,0 +1,20 @@
+package array_1;
+
+import java.util.Arrays;
+
+/**
+ * Creator: Karimov Ozodbek
+ * Date 02/02/2023
+ */
+public class MakePi {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(makePi()));
+    }
+
+    static int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
+
+}
